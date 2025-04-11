140e38b3576a1df6d9f3465685e676364ba1e948
Boma <arthurboma@yahoo.com.br>
2025-04-11 14:05:44 -0300

    Delete Html_css_JS/Luta_POO directory

--- Html_css_JS/Luta_POO/script.js
+++
@@ -1,9 +0,0 @@
-let log = new Log(document.querySelector(".log"));
-
-let char = new Knight("Boma");
-
-let monster = new BigMonster("Monstrengo");  
-
-const stage = new Stage(char, monster, document.querySelector("#char"), document.querySelector("#monster"), log);
-
-stage.start();
